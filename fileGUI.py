dee175db6efc00e78e5d262dc8694306c9073d09
Filip Zecevic <filip.zecevic@jjzmaj.edu.rs>
2023-09-19 17:20:02 +0200

    Changed the apperance, added comments and formated the code

--- fileGUI.py
+++ fileGUI.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 import customtkinter as ctk
-import pyautogui
 from tkinter import filedialog
 import calculations
 import re
@@ -10,7 +9,7 @@ import re
 #The function opens the window so the user can select a file path, it also provides the file path to the main function
 def newFile():
     global file_path
-    file_path = filedialog.asksaveasfilename(title="Select the path")
+    file_path = filedialog.asksaveasfilename(title = "Select the path")
     global file
     print(file_path)
     file = open(file_path, "w")
@@ -20,94 +19,75 @@ def newFile():
 #The Function opens the window so the user can select a file path to the exisitng file, it aslo returns the file path to the main function
 def openFile():
     global file_path
-    file_path = filedialog.askopenfilename(title="Select the path")
+    file_path = filedialog.askopenfilename(title = "Select the path")
     global file
     print(file_path)
     file = open(file_path, "r")
-    arr=[]
-    id=0
+    arr = []
+    id = 0
     with open(file_path, "r") as f:
         for line in f:
             arr.append(line.split(","))
         print(arr)
         for i in range(rows):
             for j in range(column):
-                index=column*i+j
-                if index % column ==0 or index<column:
+                index = column * i + j
+                if index % column == 0 or index < column:
                     continue
                 
-                cells_array[index].delete(0,tk.END)
-                cells_array[index].insert(0,arr[id][0])
-                id+=1
-                
-    
+                cells_array[index].delete(0, tk.END)
+                cells_array[index].insert(0, arr[id][0])
+                id += 1
 
 
 #Function that gets called after the "Save" button is pressed
 #The FUnction opens the window and lets the user choose the file path to save the file
 def saveFile():
-    file= open(file_path, "w")
-    data=get_data_from_array()
-
-    for i in range(rows-1):
-        for j in range(column-1):
-            index=(column-1)*i+j
-            
-            if (index+1) % (column-1) ==0:
+    file = open(file_path, "w")
+    data = get_data_from_array()
+    for i in range(rows - 1):
+        for j in range(column - 1):
+            index=(column - 1) * i + j
+            if (index+1) % (column - 1) == 0:
                 file.write(data[index])
             else:
-                file.write(data[index]+",")
-            
-            
-        
-        
+                file.write(data[index] + ",")
     file.close()
-    
-    
 
 
 #Function that gets called after the "Save As" button is pressed
 #The Function saves the file at file path selected by user
 def saveFileAs():
     newFile()
-    data=get_data_from_array()
-
-    for i in range(rows-1):
-        for j in range(column-1):
-            index=(column-1)*i+j
-            
-            
-            file.write(data[index]+",")
-            
-            file.write("\n")
-        
-        
+    data = get_data_from_array()
+    for i in range(rows - 1):
+        for j in range(column - 1):
+            index=(column - 1) * i + j
+            file.write(data[index] + ",")
+            file.write("\n")    
     file.close()
     print(data)
     return file_path
 
 
-def ispisi():
-    labell=tk.Label(root2, text="maj")
-    labell.pack()
+#Function that returns the anwser to equasions insicde cells
 def get_data(event,cell):
     result=cell.get()
     cell.delete(0, tk.END)  # Clear current text
     cell.insert(0, calculations.equasion(result))
     print(cell.get())
-def scroll_text(*args):
-    root2.yview(*args)
+
+
+#Function that returns the contents of cells
 def get_data_from_array():
-    lista=[]
-    content_array=[]
+    lista = []
+    content_array = []
     for i in range(rows):
         for j in range(column):
-
-            index=column*i+j
-
-            if index % column ==0 or index<column:
+            index = column * i + j
+            if index % column == 0 or index<column:
                 continue
-            if cells_array[index].get() =="":
+            if cells_array[index].get() == "":
                 content_array.append(" ")
             else:
                 content_array.append(cells_array[index].get())
@@ -115,151 +95,154 @@ def get_data_from_array():
     return content_array
 
 
+#Function that changes from home page to file page
+def goToFile():
+    home_frame.pack_forget()
+    file_frame.pack()
 
 
+#
+def goToHome():
+    file_frame.pack_forget()
+    home_frame.pack()
 
-#vezi sa prozorom
-root2=tk.Tk()
-x = root2.winfo_screenwidth()
-y = root2.winfo_screenheight()
-root2.geometry("%dx%d" % (x,y))
+#Main window where all the gui is located
+root = ctk.CTk()
+x = root.winfo_screenwidth()
+y = root.winfo_screenheight()
+root.geometry("%dx%d" % (x,y))
+root.title("SpreadSheet Calculator")
 
+#Makes the frame for the home page
+home_frame = tk.Frame(root, background = "#292430")
 
+#Home page elements
+navbar = tk.Frame(root, border = 3, width = x, background = "#1ecbe1",)
 
-root2.title("SpreadSheet Calculator")
+file_button = tk.Button(
+    navbar,
+    text = "File",
+    background = "#1ecbe1",
+    command = goToFile,
+    font = "12",
+    width=15)
 
-navbar=tk.Frame(root2,border=3, width=x,bg="lightgreen")
-label2=tk.Label(navbar, text="nav")
-navbar.config(background="green")
+home_button = tk.Button(
+    navbar,
+    text = "Home",
+    background = "#1ecbe1",
+    command = goToHome,
+    font = "12",
+    width=15)
+navbar.config(background = "#292430")
 navbar.pack(fill="x")
-label2.pack( anchor=tk.NW)
+home_button.pack(side = "left", anchor = "n")
+file_button.pack(side = "left", anchor = "n")
 
 
-label= tk.Label(
-    root2,
-    text="SpreadShit Calculator",
-    font=("Ariel",16),
-    width=x,
-    height=2
-)
+cells= tk.Frame(home_frame, height=10, width=40)
+cells.columnconfigure(0, weight=1)
+cells.columnconfigure(1, weight=1)
+cells_array = []
+dict_with_excel_index = {}
+Ascii_number = 65
+Ascii_letter = ""
+number_for_column = 1
+number_of_letters = 1
+rows = 4
+column = 10
 
-label.pack()
-cells= tk.Frame(root2, height=10, width=40,)
-
-cells.columnconfigure(0,weight=1)
-cells.columnconfigure(1,weight=1)
-cells_array=[]
-
-dict_with_excel_index={}
-
-Ascii_number=65
-Ascii_letter=""
-number_for_column=1
-number_of_letters=1
-
-rows=4
-column=10
 
 for i in range(rows):
     for j in range(column):
-        Ascii_letter=""
-        if i==0 and j!=0:
-
+        Ascii_letter = ""
+        if i == 0 and j != 0:
             for k in range(number_of_letters):
-                Ascii_letter+=chr(Ascii_number)
-
-            cell= tk.Entry(cells,width=8, font=("Arial",16),justify='center')
-            cell.insert(0,Ascii_letter)
+                Ascii_letter += chr(Ascii_number)
+            cell= tk.Entry(cells, width = 8, font=("Arial", 16), justify = 'center')
+            cell.insert(0, Ascii_letter)
+            cell.config(state = "disabled")
+            Ascii_number += 1
+            if Ascii_letter == "Z":
+                Ascii_number = 65
+                number_of_letters += 1
+        elif j == 0 and i != 0:
+            cell = tk.Entry(cells, width = 2, font = ("Arial", 16), justify = 'center')
+            cell.insert(0, number_for_column)
             cell.config(state="disabled")
-            Ascii_number+=1
-            if Ascii_letter=="Z":
-                Ascii_number=65
-                number_of_letters+=1
-
-        elif j==0 and i!=0:
-            cell= tk.Entry(cells,width=2, font=("Arial",16),justify='center')
-            cell.insert(0,number_for_column)
-            cell.config(state="disabled")
-            number_for_column+=1
-        elif j==0 and i==0:
-            cell= tk.Entry(cells,width=2, font=("Arial",16),justify='center')
-            cell.insert(2,"-")
-            cell.config(state="disabled")
+            number_for_column += 1
+        elif j == 0 and i == 0:
+            cell = tk.Entry(cells,width=2, font=("Arial",16),justify='center')
+            cell.insert(2, "-")
+            cell.config(state = "disabled")
         else:
-            cell= tk.Entry(cells,width=8, font=("Arial",16))
-        cell.grid(row=i,column=j)
-        cell.bind('<Return>', lambda event, entry=cell: get_data(event, entry))
+            cell = tk.Entry(cells, width = 8, font = ("Arial", 16))
+        cell.grid(row = i, column = j)
+        cell.bind('<Return>', lambda event, entry = cell: get_data(event, entry))
         cells_array.append(cell)
-        #
-cells.pack(side=tk.TOP, anchor=tk.NW,padx=(60,0), pady=(60,0))
-navbar.config(background="#123332")
+cells.pack(side = tk.LEFT, anchor = tk.NW, padx = (30,0), pady = (30,0))
+navbar.config(background = "#1ecbe1")
 
 
+"""button = tk.Button(home_frame, text="Get Data from Entry 12", command = lambda: get_data_from_array())
+button.pack()"""
 
-button = tk.Button(root2, text="Get Data from Entry 12", command=lambda: get_data_from_array())
-button.pack()
-
+home_frame.pack()
 #-----------------------------------------------------------------------------------------------------
 
-root = ctk.CTk(fg_color = "#292430") #Sets the root as the main window
-x = root.winfo_screenwidth()
-y = root.winfo_screenheight()
-root.attributes("-alpha", True)
-root.geometry("%dx%d" % (x,y),)
-root.title("File")
 
+#292430 -grey, #1ecbe1 - cyan
+#Frame where the file page
+file_frame = tk.Frame(root, width = x, height = y, background = "#292430")
 
-label = tk.Label(root, width=x, text="SpreadShit Calculator").pack(side="top") #Label on top of the page
+label = tk.Label(file_frame, width = x, background = "#1ecbe1", text = "Neki tekstt").pack(side = "top") #Label on top of the page
+frame = tk.Frame(file_frame, height = y, border = 5).pack(side = "left") #Makes a frame that the buttons will be placed in
 
-frame = tk.Frame(root, height=y, border=5).pack(side="left") #Makes a frame that the buttons will be placed in
-
-
-#Makes the "New" button
+#"New" button
 new_button = tk.Button(
-    frame,
-    text="New",
-    height= 3,
-    width= 50,
-    background= "#292430",
-    foreground= "#d1d1d1",
-    command=newFile,
-    ).pack(anchor="w")
+    file_frame,
+    text = "New",
+    height = 3,
+    width = 50,
+    background = "#292430",
+    foreground = "#d1d1d1",
+    command = newFile,
+    ).pack()
 
 
-#Makes the "Open" button
+#"Open" button
 open_button = tk.Button(
-    frame,
-    text="Open",
-    height= 3,
-    width= 50,
-    background= "#292430",
-    foreground= "#d1d1d1",
-    command=openFile,
-    ).pack(anchor="w")
+    file_frame,
+    text ="Open",
+    height = 3,
+    width = 50,
+    background = "#292430",
+    foreground = "#d1d1d1",
+    command = openFile,
+    ).pack()
 
 
-#Makes the "Save" button
+#"Save" button
 save_button = tk.Button(
-    frame,
-    text="Save",
-    height= 3,
-    width= 50,
-    background= "#292430",
-    foreground= "#d1d1d1",
-    command= saveFile,
-    ).pack(anchor="w")
+    file_frame,
+    text ="Save",
+    height = 3,
+    width = 50,
+    background = "#292430",
+    foreground = "#d1d1d1",
+    command = saveFile,
+    ).pack()
 
 
-#Makes the "Save As" button
+#"Save As" button
 saveas_button = tk.Button(
-    frame,
-    text="Save As",
-    height= 3,
-    width= 50,
-    background= "#292430",
-    foreground= "#d1d1d1",
-    command= saveFileAs,
-    ).pack(anchor="w")
-
+    file_frame,
+    text = "Save As",
+    height = 3,
+    width = 50,
+    background = "#292430",
+    foreground = "#d1d1d1",
+    command = saveFileAs,
+    ).pack()
 
 root.mainloop()
